5a01700024aebb5b9f682a1277dc36e6bfb3088f
Matthew <Matt@Mayateck.net>
2014-08-01 08:22:56 -0700

    Created corescript for ExtLib

---
+++ ExtLib/ExtLibrary.lua
@@ -0,0 +1,10 @@
+-- ExtendedLibrary, a Lua function library by Matt (Wehttam664/NukesForKids) at Capit Software
+-- This code is protected under the MIT license. See GitHub repository for full information.
+
+-- Modification of this code may result in dysfunctionality of the library.
+-- Support will only be offered to unmodified library files.
+
+-- Function and Extension documentation is available on the GitHub repository.
+
+ExtLibrary = {}; ExtLibrary.__index = ExtLibrary;
+lib = {}; setmetatable(ExtLibrary,lib);
